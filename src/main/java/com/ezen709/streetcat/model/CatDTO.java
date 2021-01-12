7aebf62c37eff1bf0fb58ea8677cc4e01d8d554e
jun0bae <baej0@naver.com>
2021-01-12 09:54:49 +0900

    20200112

---
+++ src/main/java/com/ezen709/streetcat/model/CatDTO.java
@@ -0,0 +1,40 @@
+package com.ezen709.streetcat.model;
+
+public class CatDTO {
+  private int cnum;
+  private String location;
+  private String iamge;
+  private String name;
+  private String feature;
+public int getCnum() {
+	return cnum;
+}
+public void setCnum(int cnum) {
+	this.cnum = cnum;
+}
+public String getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+public String getIamge() {
+	return iamge;
+}
+public void setIamge(String iamge) {
+	this.iamge = iamge;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getFeature() {
+	return feature;
+}
+public void setFeature(String feature) {
+	this.feature = feature;
+}
+  
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=euc-kr"
+    pageEncoding="EUC-KR"%>
+    
+    
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Insert title here</title>
+</head>
+<body>
+<a href="cat_board.do">게시판 가기<a>
+</body>
+</html>
